39431f1b13983be920a4bf3faafa959f989db40c
Usman10203 <udeveloper1234@gmail.com>
2025-01-09 22:32:27 +0500

    api/current

--- src/app/api/current/route.js
+++ src/app/api/current/route.js
@@ -2,13 +2,25 @@ import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 import { User } from "@/models/user";
 
-
-//basically a middleware function
 export async function GET(request) {
-  const authToken = request.cookies.get("authToken")?.value;
+  try {
+    const authToken = request.cookies.get("authToken")?.value;
 
-  const data = jwt.verify(authToken, process.env.JWT_KEY);
-  const user = await User.findById(data._id).select("-password");
+    if (!authToken) {
+      return NextResponse.json(
+        { error: "Authentication token is missing" },
+        { status: 401 }
+      );
+    }
 
-  return NextResponse.json(user);
+    const data = jwt.verify(authToken, process.env.JWT_KEY);
+    const user = await User.findById(data._id).select("-password");
+
+    return NextResponse.json(user);
+  } catch (error) {
+    return NextResponse.json(
+      { error: error.message || "Something went wrong" },
+      { status: 500 }
+    );
+  }
 }
